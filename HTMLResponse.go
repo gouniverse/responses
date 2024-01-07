7502b4b7532722b502be1d3649334b26d7c58edc
Milan Lesichkov <lesichkovm@gmail.com>
2024-01-07 17:27:43 +0000

    Update HTMLResponse.go

--- HTMLResponse.go
+++ HTMLResponse.go
@@ -4,6 +4,6 @@ import "net/http"
 
 // HTMLResponse - responds with the HTML body
 func HTMLResponse(w http.ResponseWriter, r *http.Request, body string) {
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(body))
 }
