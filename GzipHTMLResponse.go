62f062750d91ef9debc1d1f3d00b983da447396a
Milan Lesichkov <lesichkovm@gmail.com>
2024-01-17 01:39:05 +0000

    Added content type override

--- GzipHTMLResponse.go
+++ GzipHTMLResponse.go
@@ -7,7 +7,12 @@ import (
 
 // GzipHTMLResponse - responds with the HTML body
 func GzipHTMLResponse(w http.ResponseWriter, r *http.Request, body string) {
-	w.Header().Set("Content-Type", "text/html")
+	contentType := w.Header().Get("Content-Type")
+
+	if contentType == "" {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	}
+
 	w.Header().Add("Content-Encoding", "gzip")
 	gz := gzip.NewWriter(w)
 	gz.Write([]byte(body))
--- HTMLResponse.go
+++ HTMLResponse.go
@@ -4,6 +4,11 @@ import "net/http"
 
 // HTMLResponse - responds with the HTML body
 func HTMLResponse(w http.ResponseWriter, r *http.Request, body string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	contentType := w.Header().Get("Content-Type")
+
+	if contentType == "" {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	}
+
 	w.Write([]byte(body))
 }
